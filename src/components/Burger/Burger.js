cc1a8a680766c88111f26aad2e52e0099e0a988c
robinbshrestha <robinbshrestha@gmail.com>
2019-06-22 17:41:31 -0400

    add logic for when ingredients are empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => { //transforms an obj of kv pairs into array of ingredients
                 return <BurgerIngredient key={igKey + i} type={igKey} /> //where value of obj is important for how many ingredients i need and key is impt for the type of ingredient i need
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el) //flattening the array
+        }, [])
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients! </p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
