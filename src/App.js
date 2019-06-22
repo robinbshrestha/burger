ad0e172cba4840dc84ae929bbd4f2e357f5f8303
robinbshrestha <robinbshrestha@gmail.com>
2019-06-22 16:32:45 -0400

    adjust layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
   render() {
     return (
       <div>
-   
+          <Layout> 
+            <BurgerBuilder />
+          </Layout>
       </div>
     );
   }
 }
 
 export default App;
+
+//Layout component is the wrapper implementing the toolbar and burger builder
+//we'll take adv of layout where the toolbar will always be there and be able to switch the page
+//we could use app.js as the root component but we will use this to focus on the layout
+
+
+//containers are state full components - manage state
+//components are dumb and presentational components - don't manage state
+
+// layout is to wrap other components with that specific layout
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
+
+const layout = (props) => (
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
+
+//div is the placeholder for the toolbar, sidedrawer and backdrop
+//under there is the main area (the normal main element provided by html)
+//which will we will output the component we wrap with this layout
+
+//we will create a aux HOC to wrap something to make it work
+
