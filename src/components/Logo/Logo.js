25b92964760b7e08cabc80a84ca163abf4a9b621
robinbshrestha <robinbshrestha@gmail.com>
2019-06-22 22:37:52 -0400

    adjust logo styling

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import burgerLogo from '../../assets/images/burgerLogo.png';
-import classes from '../Logo.css' 
+import classes from './Logo.css';
 
 const logo = (props) => (
     <div className={classes.Logo}>
