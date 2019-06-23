161b90207b075816ff1269a8a36dae11a5f3ef9e
robinbshrestha <robinbshrestha@gmail.com>
2019-06-22 23:34:41 -0400

    adjust grayout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,11 +5,20 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
     render () {
         return (
             <Aux>
                 <Toolbar />
-                <SideDrawer />
+                <SideDrawer 
+                open={this.state.showSideDrawer}
+                closed={this.sideDrawerClosedHandler}/>
                 <div>Toolbar, SideDrawer, Backdrop</div>
                 <main className={classes.Content}>
                     {this.props.children}
