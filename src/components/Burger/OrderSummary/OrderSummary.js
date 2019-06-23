e10f959da368eadff410e2f1f2d26767c2ab9481
robinbshrestha <robinbshrestha@gmail.com>
2019-06-22 22:09:37 -0400

    modal buttons

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,6 +17,9 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to Checkout?</p>
+            <button>CANCEL</button>
+            <button>CONTINUE</button>
+
         </Aux>
     )
 };
