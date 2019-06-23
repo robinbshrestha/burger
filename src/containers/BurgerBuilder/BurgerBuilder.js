4cac9c11a74748bb9418b3130f8e49aac9ece73a
robinbshrestha <robinbshrestha@gmail.com>
2019-06-22 21:08:26 -0400

    add backdrop and styling

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,7 +25,12 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchaseable: false
+        purchaseable: false,
+        purchasing: false
+    }
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true})
     }
 
     updatePurchaseState(ingredients) {
@@ -83,7 +88,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -93,6 +98,7 @@ class BurgerBuilder extends Component {
                     disabled={disabledInfo} 
                     price={this.state.totalPrice}
                     purchaseable={this.state.purchaseable}
+                    ordered={this.purchaseHandler}
                     />
             </Aux>
         );
