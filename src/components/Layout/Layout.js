f97e70720fa9ad651a1c6d95b4e25c904cc5395e
robinbshrestha <robinbshrestha@gmail.com>
2019-06-22 23:30:49 -0400

    change layout to class

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
-
-export default layout;
+class Layout extends Component {
+    render () {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer />
+                <div>Toolbar, SideDrawer, Backdrop</div>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+} 
+export default Layout;
 
 //div is the placeholder for the toolbar, sidedrawer and backdrop
 //under there is the main area (the normal main element provided by html)
